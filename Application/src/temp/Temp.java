38c70d1fb1fb206aafb31625a1419ea47a8cfb21
Jacky_Yeung <jackyyeung1022@gmail.com>
2024-10-16 22:05:47 +0800

    1

--- Application/src/temp/Temp.java
+++ Application/src/temp/Temp.java
@@ -3,5 +3,8 @@ package temp;
 public class Temp {
 	public static void main(String[] args) {
 		System.out.println("IN eclipse test");
+		System.out.println("Ethan testing");
+		System.out.println("nmsl");
+		System.out.println("jacky testing");
 	}
 }
